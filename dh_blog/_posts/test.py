540075b913076c356ee9be74a8f5e127d6e2f088
xiejia1995 <rg194@uni-heidelberg.de>
2022-06-23 18:13:24 +0200

    Delete test.py

--- dh_blog/_posts/test.py
+++
@@ -1,7 +0,0 @@
-dinner = ['Carl','Happy','Luke','Danny','Ziggy','Dom','Jerry']
-#print ("sry, i can not")
-for guest in dinner:
-    print (dinner.pop())
-    popped_name = dinner.pop()
-    
-    #print('sry'+ popped_name.title() + 'i cannot')
